9ca38cf65a6a659f24342299348048317e22a061
wilhamsJW <wilhams.dev@gmail.com>
2021-12-26 14:14:14 -0300

    style(Header): styled-header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,23 @@
+import Link from 'next/link'
+
 import styles from './styles.module.scss'
 
 export function Header() {
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <img src="/img/logo.svg" alt="Logo my Board" /> {/** Com next não precisa colocar o caminho exato pra buscar a img, ele é inteligente o suficiente p entender */}
-                <a>Home</a>
-                <a>My Board</a>
-
+                <Link href="/">
+                    <img src="/img/logo.svg" alt="Logo my Board" /> {/** Com next não precisa colocar o caminho exato pra buscar a img, ele é inteligente o suficiente p entender */}
+                </Link>
+                <nav>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                    <Link href="/board">
+                        <a>My Board</a>
+                    </Link>
+                    
+                </nav>
                 <button>
                     Log in with GitHub
                 </button>
