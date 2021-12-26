2a1a88b6d90e4f0975a8546924470a9c7723a929
wilhamsJW <wilhams.dev@gmail.com>
2021-12-26 14:17:40 -0300

    bug(Header): multiple-children

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,8 +6,17 @@ export function Header() {
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
+                {/** Com next não precisa colocar o caminho exato pra buscar a img, ele é inteligente o suficiente p entender 
+                 * 
+                 * OBS: Havia um erro chamado: "Multiple children were passed to
+                    Why This Error Occurred"
+                    isso ocorreu pq havia um comentário dentro da tag LINK e ele entendeu como outro elemento
+                    ou seja não é suportado vários filhos ou tags dentro de uma mesma TAG <LINK />
+
+                    https://nextjs.org/docs/messages/link-multiple-children
+                */}
                 <Link href="/">
-                    <img src="/img/logo.svg" alt="Logo my Board" /> {/** Com next não precisa colocar o caminho exato pra buscar a img, ele é inteligente o suficiente p entender */}
+                    <img src="/img/logo.svg" alt="Logo my Board" />
                 </Link>
                 <nav>
                     <Link href="/">
